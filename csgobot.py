ebd74af53196bc4ffeb01bbc01ccdf399ad6dc95
Charlie Rodiger <charrod796@gmail.com>
2016-01-25 00:51:08 -0500

    Created CSGOBot

---
+++ csgobot.py
@@ -0,0 +1,32 @@
+import praw
+import time
+import os
+import urllib2
+import re
+def scrape(team):
+    req = urllib2.Request('http://www.hltv.org/?pageid=152&query='+team, headers={'User-Agent' : "Magic Browser"})
+    con = urllib2.urlopen( req )
+    source = con.read()
+    players = re.findall(r'">(.*?)</a></td>',source)
+    return players
+
+def get_team(comment):
+    comment = comment.lower().split()
+    for num in range(len(list)):
+        if list[num] == '!roster' or '!team':
+            return list[num+1]
+
+r = praw.Reddit('An easy way to access team rosters.')
+r.login(os.environ['REDDIT_USER'], os.environ['REDDIT_PASS'])
+rcall = ['!roster', '!team']
+already_done = []
+while True:
+    subreddit = r.get_subreddit('globaloffensive')
+    comments = subreddit.get_comments()
+    flat_comments = praw.helpers.flatten_tree(comments)
+    for comment in comments:
+        has_call = any(string in comment.body for string in rcall)
+        if comment.id not in already_done and has_call:
+            comment.reply(scrape(get_team(comment)))
+            already_done.append(comment.id)
+    time.sleep(600)
