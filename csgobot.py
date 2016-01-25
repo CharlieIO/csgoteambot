49ba3b5b612e7b63bb36dfb353613c7e1025c7de
Charlie Rodiger <charrod796@gmail.com>
2016-01-25 12:08:49 -0500

    Bug fixes

--- csgobot.py
+++ csgobot.py
@@ -37,7 +37,7 @@ while True:
         if comment.id not in already_done and has_call:
             team = get_team(comment)
             members = scrape(team)
-            format_text = '\n**Roster**|\n %s\n*'*len(members)
+            format_text = '\n**Roster**|\n' + ('\n%s\n*'*len(members))
             comment.reply('Information for **'+team.upper()+'**' + format_text % tuple(members))
             already_done.append(comment.id)
             print "Comment posted"
