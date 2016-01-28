154e366b10b37631938ba3d91e91e81e218cef26
Charlie Rodiger <charrod796@gmail.com>
2016-01-27 21:56:21 -0500

    Re-did the way teams are formed. Should provide more accurate results.

--- csgobot.py
+++ csgobot.py
@@ -11,6 +11,32 @@ urlparse.uses_netloc.append("postgres")
 url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
 
+def auto_scrape():
+    tcount = 0
+    pcount = 0
+    teamlink = teamlinks()
+    for tname in teamlink:
+        players_updated = False
+        tlink = teamlink[tname]
+        players, playerlink = tscrape(teamlink[tname])
+        win, draw, loss, mapsplayed, kills, deaths, rounds, kdratio = statscrape(teamlink[tname])
+        for player in playerlink:
+            plink = playerlink[player]
+            pname, age, team, k, hsp, d, rating, p1, p2, p3, p4 = pstats(playerlink[player])
+            if not players_updated:
+                team_database_update(team, p1, p2, p3, p4, player, win, draw, loss, rounds, tlink)
+                players_updated = True
+                tcount += 1
+                print team + ' has been modified. \n' + str(tcount) + ' teams modified.'
+            player_database_update(player, pname, age, team, k, d, hsp, rating, plink)
+            pcount += 1
+            print player + ' has been modified. \n' + str(pcount) + ' players modified. #' + team
+            time.sleep(3)
+            # name, age, team, K, HSP, D, Rating
+        time.sleep(3)
+    print 'UPDATE COMPLETE :)'
+
+
 def teamlinks():
     namelink = {}
     req = urllib2.Request('http://www.hltv.org/?pageid=182&mapCountOverride=10', headers={
@@ -31,6 +57,7 @@ def pstats(plink):
     source = con.read()
     soup = BeautifulSoup(source, 'html.parser')
     stats = []
+    names = []
     personalstats = []
     for stat in soup.find_all(style="font-weight:normal;width:100px;float:left;text-align:right;color:black"):
         if '%' in stat.get_text():
@@ -39,64 +66,37 @@ def pstats(plink):
             stats += [stat.get_text()]
     for stat in soup.find_all(style="font-weight:normal;width:185px;float:left;text-align:right;color:black;"):
         if '-' == stat.get_text():
-            personalstats += [stat.get_text().replace('-', '')]
+            personalstats += [stat.get_text().replace('-', '99')] #fix for no age listed
         else:
             personalstats += [stat.get_text()]
-    for stat in soup.find_all(style="font-weight:normal;width:100px;float:left;text-align:right;color:black;font-weight:bold"):
+    for stat in soup.find_all(
+            style="font-weight:normal;width:100px;float:left;text-align:right;color:black;font-weight:bold"):
         stats += [stat.get_text()]
-    return personalstats[0], personalstats[1], personalstats[3], stats[0], stats[1], stats[2], stats[9]
-    # name, age, team, K, HSP, D, Rating
+    for name in soup.find_all('b'):
+        if name.get_text()[0] == "'":
+            names += [name.get_text().strip("'")]
+    if len(names) == 4:
+        print names
+        return personalstats[0], personalstats[1], personalstats[3], stats[0], stats[1], stats[2], stats[9], names[0], names[1], names[2], names[3]
+    else:
+        print 'no team'
+        return personalstats[0], personalstats[1], personalstats[3], stats[0], stats[1], stats[2], stats[9], '', '', '', ''
 
+def tscrape(teamlink):
+    players = []
+    req = urllib2.Request('http://www.hltv.org/' + teamlink, headers={
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36"})
+    con = urllib2.urlopen(req)
+    source = con.read()
+    soup = BeautifulSoup(source, 'html.parser')
+    playerlink = {}
+    for link in soup.find_all('a', href=True):
+        if re.findall('pageid=173', link['href']):
+            if '(' in link.get_text():
+                playerlink[link.get_text().split(' (')[0]] = link.get('href')
+                players += [link.get_text().split(' (')[0]]
+    return players, playerlink  # every player that has been on a team.
 
-def show_table():
-    conn = psycopg2.connect(
-            database=url.path[1:],
-            user=url.username,
-            password=url.password,
-            host=url.hostname,
-            port=url.port
-    )
-    cur = conn.cursor()
-    print 'connected'
-    cur.execute("SELECT * FROM CSGO_TEAMS")
-    print 'changes completed'
-    #cur.execute("ALTER TABLE CSGO_PLAYERS ALTER COLUMN HSP SET DATA TYPE NUMERIC (3,1)")
-    # rows = cur.fetchall()
-    # print "\nShow me the databases:\n"
-    # for row in rows:
-    #     print "   ", row
-    conn.commit()
-    print 'done'
-    conn.close()
-
-
-def auto_scrape():
-    tcount = 0
-    pcount = 0
-    teamlink = teamlinks()
-    for name in teamlink:
-        link = teamlink[name]
-        p1, p2, p3, p4, p5, playerlink = tscrape(teamlink[name])
-        win, draw, loss, mapsplayed, kills, deaths, rounds, kdratio = statscrape(teamlink[name])
-        team_database_update(name, p1, p2, p3, p4, p5, win, draw, loss, rounds, link)
-        tcount += 1
-        print name + ' has been modified. \n' + str(tcount) + ' teams modified.'
-        for p in playerlink:
-            player = p
-            link = playerlink[p]
-            print link
-            name, age, team, k, hsp, d, rating = pstats(playerlink[p])
-            player_database_update(player, name, age, team, k, d, hsp, rating, link)
-            pcount += 1
-            print player + ' has been modified. \n' + str(pcount) + ' players modified.'
-
-            time.sleep(3)
-            # name, age, team, K, HSP, D, Rating
-        time.sleep(3)
-
-
-
-    print 'UPDATE COMPLETE :)'
 
 def player_database_update(player, name, age, team, k, d, hsp, rating, link):
     conn = psycopg2.connect(
@@ -107,9 +107,9 @@ def player_database_update(player, name, age, team, k, d, hsp, rating, link):
             port=url.port
     )
     cur = conn.cursor()
-    cur.execute("SELECT PLAYER FROM CSGO_PLAYERS")
-    nameslist = cur.fetchall()
-    if (player.encode('latin1'),) not in nameslist:
+    cur.execute("SELECT LINK FROM CSGO_PLAYERS")
+    linklist = cur.fetchall()
+    if (link.encode('latin1'),) not in linklist:
         cur.execute(
                 "INSERT INTO CSGO_PLAYERS (PLAYER, IRLNAME, AGE, TEAM, KILLS, DEATHS, HSP, RATING, LINK) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
                 (player, name, age, team, k, d, hsp, rating, link))
@@ -161,25 +161,6 @@ def team_database_update(tname, p1, p2, p3, p4, p5, win, draw, loss, rounds, lin
     conn.close()
 
 
-def tscrape(teamlink):
-    players = []
-    req = urllib2.Request('http://www.hltv.org/' + teamlink, headers={
-        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36"})
-    con = urllib2.urlopen(req)
-    source = con.read()
-    soup = BeautifulSoup(source, 'html.parser')
-    playerlink = {}
-    counter = 0
-    while counter < 5:
-        for link in soup.find_all('a', href=True):
-            if re.findall('pageid=173', link['href']) and counter < 5:
-                if '(' in link.get_text() and counter < 5:
-                    playerlink[link.get_text().split(' (')[0]] = link.get('href')
-                    players += [link.get_text().split(' (')[0]]
-                    counter += 1
-    return players[0], players[1], players[2], players[3], players[4], playerlink  # top 5 players by rounds played
-
-
 def statscrape(teamlink):
     req = urllib2.Request('http://www.hltv.org/' + teamlink, headers={
         'User-Agent': "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36"})
@@ -202,11 +183,36 @@ def get_team(comment):
         if comment[num] == '!roster' or comment[num] == '!team':
             return str(comment[num + 1])
 
+
+def show_table():
+    conn = psycopg2.connect(
+            database=url.path[1:],
+            user=url.username,
+            password=url.password,
+            host=url.hostname,
+            port=url.port
+    )
+    cur = conn.cursor()
+    print 'connected'
+    cur.execute("SELECT * FROM CSGO_TEAMS")
+    print 'changes completed'
+    # cur.execute("ALTER TABLE CSGO_PLAYERS ALTER COLUMN HSP SET DATA TYPE NUMERIC (3,1)")
+    # rows = cur.fetchall()
+    # print "\nShow me the databases:\n"
+    # for row in rows:
+    #     print "   ", row
+    conn.commit()
+    print 'done'
+    conn.close()
+
+
 auto_scrape()
 # r = praw.Reddit('An easy way to access team rosters.')
 # r.login(os.environ['REDDIT_USER'], os.environ['REDDIT_PASS'])
 # rcall = ['!roster', '!team']
-# already_done = []
+# pcall = ['!player', '!rektby']
+# talready_done = []
+# palready_done = []
 # forbidden = '+%\\*";[]{}:'
 # while True:
 #     conn = psycopg2.connect(
@@ -222,8 +228,9 @@ auto_scrape()
 #     flat_comments = praw.helpers.flatten_tree(comments)
 #     for comment in flat_comments:
 #         print comment
-#         has_call = rcall[0] in comment.body or rcall[1] in comment.body
-#         if comment.id not in already_done and has_call:
+#         has_team_call = rcall[0] in comment.body or rcall[1] in comment.body
+#         has_player_call = rcall[0] in comment.body or rcall[1] in comment.body
+#         if comment.id not in already_done and has_team_call:
 #             team = get_team(comment.body)
 #             statfill = '\n\n**Wins:** %s' + '\n\n**Draws:** %s' + '\n\n**Losses:** %s' + '\n\n**Rounds Played:**  %s'
 #             if team != '!roster' and team != '!team' and any((c in forbidden) for c in team) == -1:
@@ -258,7 +265,44 @@ auto_scrape()
 #                 except:
 #                     print '~~~~~~ERROR3~~~~~~'
 #                     pass
-#                 already_done.append(comment.id)
+#                 talready_done.append(comment.id)
 #             print "~~~~~~~~~Comment posted.~~~~~~~~~"
+#         if comment.id not in already_done and has_team_call:
+#             team = get_team(comment.body)
+#             statfill = '\n\n**Wins:** %s' + '\n\n**Draws:** %s' + '\n\n**Losses:** %s' + '\n\n**Rounds Played:**  %s'
+#             if team != '!roster' and team != '!team' and any((c in forbidden) for c in team) == -1:
+#                 try:
+#                     if team.upper() == 'VP':
+#                         team.replace('VP', 'Virtus.Pro')
+#                     cur.execute("SELECT * FROM CSGO_TEAMS WHERE UPPER(TEAM_NAME) LIKE UPPER((%s)) LIMIT 1",
+#                                 ('%' + team + '%',))
+#                     stats = cur.fetchall()
+#                     print stats
+#                     tstats = stats[0][6:10]
+#                     players = stats[0][1:6]
+#                     team = stats[0][0]
+#                     link = stats[0][10]
+#                     print players
+#                 except:
+#                     print '~~~~~~ERROR1~~~~~~'
+#                     pass
+#                 try:
+#                     format_text = '\n\nPlayer | Rating ' + '\n:--:|:--:' + (
+#                     '\n%s | Rating will be added soon.' * 5) + (
+#                                       statfill % (tuple(tstats))) + '\n\n**Win/Loss Ratio:** ' + str(
+#                             round((float(tstats[0]) / float(tstats[2])), 2))
+#                 except:
+#                     print '~~~~~~ERROR2~~~~~~'
+#                     pass
+#                 try:
+#                     comment.reply(
+#                             'Information for **' + team.replace('&nbsp;', '').replace('%20', ' ').upper() + '**:' + (
+#                                 format_text % (
+#                                 tuple(players))) + '\n\n [Powered by HLTV](http://www.hltv.org/' + link + ') \n\n [GitHub Source](https://github.com/Charrod/csgoteambot)')
+#                 except:
+#                     print '~~~~~~ERROR3~~~~~~'
+#                     pass
+#                 talready_done.append(comment.id)
+#             print "~~~~~~~~~Player Comment posted.~~~~~~~~~"
 #     conn.close()
 #     time.sleep(20)
